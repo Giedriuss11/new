401cd3f98b949ed200f860a796b773510fc48b8e
Giedriuss11 <124603399+Giedriuss11@users.noreply.github.com>
2023-02-06 21:00:26 +0200

    first commit

--- new.js
+++ new.js
@@ -1,8 +1,18 @@
 
 
 const button =document.querySelector("button")
+const main =document.querySelector(".main")
 
 
 button.onclick = () => {
-    console.log("asdasd")
+
+    main.innerHTML += `
+        <div class="card">
+            <h1>Giedrius</h1>
+            <h3>Age: 27 years</h3>
+            <h3>Hair color: Dirty blonde</h3>
+            <img src="https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/img.jpg" alt="">
+        </div>
+    `
+
 }
